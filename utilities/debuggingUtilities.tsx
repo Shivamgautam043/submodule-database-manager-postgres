89de748c353b1b926f82e1ad4fd3960f292fe76c
Shivam <112778663+Shivamgautam043@users.noreply.github.com>
2024-12-08 00:50:49 +0530

    Create debuggingUtilities.tsx

---
+++ utilities/debuggingUtilities.tsx
@@ -0,0 +1,59 @@
+export function getFormDataRequestInLoggableForm(request: Request, formData: FormData, errorCode: string, error?: string): string {
+    return JSON.stringify({
+        errorCode: errorCode,
+        error: error,
+        url: request.url,
+        headers: getRequestHeadersAsArray(request),
+        body: getRequestFormDataAsArray(formData),
+    });
+}
+
+export function getJsonRequestInLoggableForm(request: Request, bodyJson: unknown, errorCode: string, error?: string): string {
+    return JSON.stringify({
+        errorCode: errorCode,
+        error: error,
+        url: request.url,
+        headers: getRequestHeadersAsArray(request),
+        body: bodyJson,
+    });
+}
+
+export function getTextRequestInLoggableForm(request: Request, bodyText: unknown, errorCode: string, error?: string): string {
+    return JSON.stringify({
+        errorCode: errorCode,
+        error: error,
+        url: request.url,
+        headers: getRequestHeadersAsArray(request),
+        body: bodyText,
+    });
+}
+
+export function getRequestHeadersAsArray(request: Request): Array<[string, string]> {
+    const headers: Array<[string, string]> = [];
+
+    for (const kvp of request.headers.entries()) {
+        headers.push(kvp);
+    }
+
+    return headers;
+}
+
+export function getRequestFormDataAsArray(formData: FormData): Array<[string, string]> {
+    const body: Array<[string, string]> = [];
+
+    for (const [k, v] of formData.entries()) {
+        body.push([k, JSON.stringify(v)]);
+    }
+
+    return body;
+}
+
+export function getRequestFormDataAsDictionary(formData: FormData): Record<string, string> {
+    const body: Record<string, string> = {};
+
+    for (const [k, v] of formData.entries()) {
+        body[k] = v;
+    }
+
+    return body;
+}
