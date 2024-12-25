5fb2ad92420d63f7f4af1f7f1255bbacbff4f1d4
Shivam <gautamshivam043@gmail.com>
2024-12-26 01:17:09 +0530

    fixes

--- typeDefinitions.tsx
+++ typeDefinitions.tsx
@@ -1,17 +1,16 @@
-export type Uuid = string & {__brand: "Uuid"};
+export type Uuid = string & { __brand: "Uuid" };
 
-export type NonEmptyString = string & {__brand: "NonEmptyString"};
+export type NonEmptyString = string & { __brand: "NonEmptyString" };
 
-export type Iso8601DateTime = string & {__brand: "Iso8601DateTime"};
-export type Iso8601Date = string & {__brand: "Iso8601Date"};
-export type Iso8601Time = string & {__brand: "Iso8601Time"};
+export type Iso8601DateTime = string & { __brand: "Iso8601DateTime" };
+export type Iso8601Date = string & { __brand: "Iso8601Date" };
+export type Iso8601Time = string & { __brand: "Iso8601Time" };
 
-export type JwtString = string & {__brand: "JwtString"};
+export type JwtString = string & { __brand: "JwtString" };
 
 export type Integer = number;
 export type RealNumber = number;
 
-// Hex color code of the form `#xxxxxx`
 export type ColorHexCodeNormalized = string;
 export type BlurHash = string;
 
